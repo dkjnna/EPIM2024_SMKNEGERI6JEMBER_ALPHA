d9cdb884d464a118435cf7029fb26fb2ce85fb77
dkjnna <faridfadilah70@gmail.com>
2024-08-31 10:01:31 +0700

    commit dila

---
+++ includes/about.php
@@ -0,0 +1,37 @@
+<div class="" id="about">
+    <div class="about">
+        <img src="assets/images/image.png" alt="" class="foto-about">
+        <div class="content-about">
+            <div class="desc">
+                <h1>ABOUT CONCERT</h1>
+                <h2>BRUNO MARS TOUR FOR THE LATEST ALBUM - ‘24K MAGIC’</h2>
+                <p>Bruno Mars will light up Jakarta International Stadium on September 13th and 14th, 2024, as part of his world tour. Experience his electrifying performances of chart-topping hits like "Uptown Funk" and "Just the Way You Are." This is your chance to witness a spectacular evening filled with energy, music, and Bruno Mars' signature showmanship. Get your  tickets now on the official Bruno on Stage (BOS) website before they sell out!</p>
+                <div class="more">
+                    <div>
+                        <h4>Place</h4>
+                        <p>Jakarta International Stadium. Papanggo, District. Tj. Priok, North Jkt, Special Capital Regi on of Jakarta</p>
+                    </div>
+                    <div>
+                        <h4>Date</h4>
+                        <p>September 13th-14th, 2024</p>
+                    </div>
+                    <div>
+                        <h4>Time</h4>
+                        <p>10.00 PM - 1.00 AM</p>
+                    </div>
+                </div>
+            </div>
+            <div class="futher">
+                <div class="judul">
+                    <h1>Futher Information</h1>
+                </div>
+                <div class="btn">
+                    <button>Terms</button>
+                    <button>Safety</button>
+                    <button>Age</button>
+                    <button>Security</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -4,11 +4,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
-    <!-- dilaa -->
+    <div class="container">
 
+    <?php require("includes/navbar.php");?>
+        <!-- dilaa -->
+    <div class="home" id="home">
+        <img src="assets/images/bg.jfif" alt="" class="bg-home">
+        <div class="foto-home">
+            <img src="assets/images/bruno.png" alt="" class="foto-bruno">
+        </div>
+        <div class="content-home">
+            <h2 class="subtitle">Coming soon</h2>
+            <h1 class="title">Bruno On Stage</h1>
+            <h4 class="location">Jakarta International Stadium</h4>
+            <div class="informasi">
+                <p>Tour For  The latest Album – ‘24kmagic’</p>
+                <p>Sunday, September 13th-14th, 2024</p>
+            </div>
+        </div>
+    </div>
+    <div class="c" style="background-color: white; justify-content: center; align-items: center;">
+    <div class="subhome">
+        <div class="content1">
+            <div class="deskripsi">
+                <div>
+                    <h1>The Bruno’s Concert</h1>
+                    <P>Get ready for an unforgettable night as the sensational 
+                        Bruno Mars takes the stage on his highly anticipated world 
+                        tour! Known for his electrifying performances and 
+                        chart-topping hits, Bruno Mars promises an evening filled 
+                        with energy, emotion, and extraordinary music. Join us for 
+                        an exhilarating journey through his greatest hits and 
+                        experience the magic of live music like never before. 
+                        This is a concert event you won’t want to miss!</P>
+                </div>
+                <a href="" class="button">Get Ticket</a>
+            </div>
 
+            <div class="album">
+                <div class="judul-album">
+                    <span>A</span>
+                    <span>L</span>
+                    <span>B</span>
+                    <span>U</span>
+                    <span>M</span>
+                </div>
+                <div class="foto-album">
+                    <img src="assets/images/24k.jpg" alt="" class="satu">
+                    <img src="assets/images/monyet.jpg" alt="" class="dua">
+                    <img src="assets/images/roket.jpg" alt="" class="tiga">
+                    <img src="assets/images/sonic.jpg" alt="" class="empat">
+                </div>
+            </div>
+        </div>
+
+        <div class="content2">
+            <img src="assets/images/bruno-gitar.jpg" alt="" class="foto-bruno">
+            <div class="deskripsi">
+                <h1>BRUNO MARS</h1>
+                <p>is now back to entertain us with his beautiful songs on tour.</p>
+                <h2>Get Your Own Ticket Now</h2>
+            </div>
+        </div>
+    </div>
+
+    <?php include("includes/about.php");?>
+    </div>
 
 
 
@@ -20,5 +84,6 @@
 
 
     
+    </div>
 </body>
 </html>
